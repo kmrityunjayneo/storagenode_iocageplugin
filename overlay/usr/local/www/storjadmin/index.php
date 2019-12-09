8b01bb4e45aa6a9fb82eb9d612255b1a0eb10a88
Mrityunjay <kmrityunjay@gmail.com>
2019-12-09 17:12:41 +0530

    Updated the Ngnix Path and Message of the Day (motd)

--- overlay/usr/local/www/storjadmin/index.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$out = shell_exec("bash sample.sh fileA file2 fileCDE 2>&1 ");
-echo "Sample Shell run : <br>" . $out ;
-phpinfo();
-
-?>
--- overlay/usr/local/www/storjadmin/sample.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/local/bin/bash
-for file in $*
-do
-   echo "Processing file <b> $file </b> <br>"
-done
